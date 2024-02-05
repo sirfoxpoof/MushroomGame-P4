334f9d99dd38140ced7420d67763f2ba45923b6c
sirfoxpoof <daniquedrost4@gmail.com>
2024-02-05 23:44:02 +0100

    Project push

---
+++ Assets/Testing/nien dr shit/Frank.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frank : MonoBehaviour
+{   
+    public GameObject FrankText;
+    public void GetFranked()
+    {   
+        FrankText.SetActive(true);
+    }
+}
