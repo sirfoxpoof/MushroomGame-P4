0b071ec08e9d2f08237db791b412280af79a9124
Suus2005 <124685426+Suus2005@users.noreply.github.com>
2024-06-28 16:13:21 +0200

    Ehm ja forgot

---
+++ Assets/Scripts/AnimationEventTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimEventTest (float i)
+    {
+        Debug.Log("Animation Event! (" + i.ToString() + ")");
+    }
+}
